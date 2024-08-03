d4c116cd30d62a829a3639a943428109a72a8270
Ela <micaelafrancisco13@outlook.com>
2024-08-03 17:25:06 +0800

    seed the Categories table

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -8,4 +8,14 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     // creates a category table
     // type is the table name
     public DbSet<Category> Categories { get; set; }
+    
+    // seed the Categories table
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+            new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+            new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+        );
+    }
 }
