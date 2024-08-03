97319caf38b4dad490b809eea43668d2b7e715f7
Ela <micaelafrancisco13@outlook.com>
2024-08-03 17:08:07 +0800

    create the Category database via EF

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bulky.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+}
