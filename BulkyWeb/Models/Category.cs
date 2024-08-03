e7391c475a3cd43bfbf56d5d9366d639add8bb5f
Ela <micaelafrancisco13@outlook.com>
2024-08-03 16:58:26 +0800

    create the Category model

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models;
+
+public class Category
+{
+    // primary key
+    // [Key] is not required if key's name is Id or CategoryId
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
