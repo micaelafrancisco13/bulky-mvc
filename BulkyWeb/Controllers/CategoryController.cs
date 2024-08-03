806d5aa7bff6a2c08fb7344f3600f14579c1ae5b
Ela <micaelafrancisco13@outlook.com>
2024-08-03 18:04:27 +0800

    inject the application database context to the Category controller

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers;
 
 public class CategoryController : Controller
 {
+    private readonly ApplicationDbContext _dbContext;
+    
+    // dbContext is the context from AddDbContext on Program.cs.
+    // it is injected via DI.
+    public CategoryController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
     // GET
     public IActionResult Index()
     {
+        var objCategoryList = _dbContext.Categories.ToList();
         return View();
     }
 }
